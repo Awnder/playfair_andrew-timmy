4af549e21c669194bcb1e4254529ac9728aa3056
Awnder <amshiroma@zoho.com>
2024-02-10 20:11:05 -0800

    Completed rectangle_shift

--- playfair_project.py
+++ playfair_project.py
@@ -52,11 +52,12 @@ class Playfair:
                 return (row, grid[row].index(letter))
         return None
 
-    def get_rectangle_shift(pos1, pos2, encrypt):
+    def get_rectangle_shift(pos1, pos2):
         # ANDREW
         '''
-        When two letters are not in the same column or row, this function switches them along a rectangle, where 
-        pos1 and pos2 are two letters' indecies as (row, col) tuples. encrypt is bool determining sign (encrypt/decrypt)
+        When two letters are not in same column or row, switch them along a rectangle, where pos1 and pos2 are two 
+        letters' indecies as (row, col) tuples. Bool encrypt isn't needed as function will flip letter location 
+        the same way regardless of encrypting or decrypting
         '''
         new_letter1_col = -1
         new_letter2_col = -1
@@ -66,22 +67,16 @@ class Playfair:
         letter2_col = pos2[1]
 
         col_difference = abs(letter1_col - letter2_col)
-        
-        if letter1_row > letter2_row:
-            if encrypt:
-                new_letter1_col = letter1_col + col_difference
-                new_letter2_col = letter2_col - col_difference
-            else:
-                new_letter1_col = letter1_col - col_difference
-                new_letter2_col = letter2_col + col_difference
+
+        print((letter1_row,letter1_col),(letter2_row,letter2_col))
+        if letter1_col < letter2_col:
+            new_letter1_col = letter1_col + col_difference
+            new_letter2_col = letter2_col - col_difference
         else:
-            if encrypt:
-                new_letter1_col = letter1_col - col_difference
-                new_letter2_col = letter2_col + col_difference
-            else:
-                new_letter1_col = letter1_col + col_difference
-                new_letter2_col = letter2_col - col_difference
-        return
+            new_letter1_col = letter1_col - col_difference
+            new_letter2_col = letter2_col + col_difference
+            
+        return [(letter1_row, new_letter1_col), (letter2_row, new_letter2_col)]
     
     def get_column_shift(pos1, pos2, encrypt):
         # TIMMY
