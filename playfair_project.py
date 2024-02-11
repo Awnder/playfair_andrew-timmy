098a87d4487c1b8448e158fb1b82b4234ee6edaf
Awnder <amshiroma@zoho.com>
2024-02-10 17:26:15 -0800

    Rectangle shift modified

--- playfair_project.py
+++ playfair_project.py
@@ -46,20 +46,41 @@ class Playfair:
         return ''.join(digrams)
     
     def get_indecies_of_letter(grid, letter):
-        '''Returns a tuple of indecies (row, col) of an inputted letter in a given grid'''
+        '''Returns a tuple of indecies (row, col) of an inputted letter in a given grid. Returns None otherwise'''
         for row in range(len(grid)):
-            print(grid[row])
             if letter in grid[row]:
                 return (row, grid[row].index(letter))
-
+        return None
 
     def get_rectangle_shift(pos1, pos2, encrypt):
         # ANDREW
         '''
-        When two numbers are not in the same column or row, this function switches them along a rectangle, where 
-        pos1 and pos2 are a list of indecies in the grid. encrypt is bool determining sign (encrypt/decrypt)
+        When two letters are not in the same column or row, this function switches them along a rectangle, where 
+        pos1 and pos2 are two letters' indecies as (row, col) tuples. encrypt is bool determining sign (encrypt/decrypt)
         '''
+        new_letter1_col = -1
+        new_letter2_col = -1
+        letter1_row = pos1[0]
+        letter1_col = pos1[1]
+        letter2_row = pos2[0]
+        letter2_col = pos2[1]
+
+        col_difference = abs(letter1_col - letter2_col)
         
+        if letter1_row > letter2_row:
+            if encrypt:
+                new_letter1_col = letter1_col + col_difference
+                new_letter2_col = letter2_col - col_difference
+            else:
+                new_letter1_col = letter1_col - col_difference
+                new_letter2_col = letter2_col + col_difference
+        else:
+            if encrypt:
+                new_letter1_col = letter1_col - col_difference
+                new_letter2_col = letter2_col + col_difference
+            else:
+                new_letter1_col = letter1_col + col_difference
+                new_letter2_col = letter2_col - col_difference
         return
     
     def get_column_shift(pos1, pos2, encrypt):
